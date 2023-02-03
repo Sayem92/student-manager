6a58eccbed43a9cb51d75d805854b8122924a3c5
Sayem92 <sayemmiha123@gmail.com>
2023-02-03 22:07:27 +0600

    add student form

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { AddStudent } from './components/AddStudent/AddStudent';
+
+function App() {
+  return (
+    <div className="">
+      <AddStudent></AddStudent>
+   
+    
+    </div>
+  );
+}
+
+export default App;
